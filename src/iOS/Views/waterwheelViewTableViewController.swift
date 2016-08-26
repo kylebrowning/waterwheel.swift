9b8fbf6df35bcedf45e92f1d9c86a711767a489a
Kyle Browning <kylebrowning@me.com>
2016-08-26 15:29:23 -0700

    Renames waterwheelTableViewController

--- src/iOS/Views/waterwheelViewTableViewController.swift
+++ src/iOS/Views/waterwheelViewTableViewController.swift
@@ -1,36 +1,35 @@
 //
-//  waterwheelTableViewController.swift
+//  waterwheelViewTableViewController.swift
 //  waterwheel
 //
 //  Copyright © 2016 Kyle Browning. All rights reserved.
 //
 
 import UIKit
-import waterwheel
 import SwiftyJSON
 import ObjectMapper
 
-struct ViewResponseRow: Mappable {
+public struct ViewResponseRow: Mappable {
     var title: String?
     var body:  String?
 
-    init?(_ map: Map) {
+    public init?(_ map: Map) {
 
     }
 
-    mutating func mapping(map: Map) {
+    mutating public func mapping(map: Map) {
         title     <- map["title"]
         body  <- map["body"]
     }
 }
 
-class waterwheelViewTableViewControllerNew<Row: Mappable, Cell: UITableViewCell>: UITableViewController {
-    var rows: [Row] = []
+public class waterwheelViewTableViewController<Row: Mappable, Cell: UITableViewCell>: UITableViewController {
+    public var rows: [Row] = []
 
-    let reuseIdentifier = "Cell"
-    let configure: (Cell, Row) -> ()
-    var didSelect: (Row) -> () = { _ in }
-    var didFinish: (JSON?, waterwheelViewTableViewControllerNew) -> () = { _ in }
+    public let reuseIdentifier = "Cell"
+    public let configure: (Cell, Row) -> ()
+    public var didSelect: (Row) -> () = { _ in }
+    public var didFinish: (JSON?, waterwheelViewTableViewController) -> () = { _ in }
 
     init(items: [Row], configure: (Cell, Row) -> ()) {
         self.configure = configure
@@ -38,7 +37,7 @@ class waterwheelViewTableViewControllerNew<Row: Mappable, Cell: UITableViewCell>
         self.rows = items
     }
 
-    convenience init(viewPath: (String), configure: (Cell, Row) -> ()) {
+    public convenience init(viewPath: (String), configure: (Cell, Row) -> ()) {
         self.init(items: [], configure: configure)
         waterwheel.viewGet(viewPath: viewPath, completionHandler:  { (success, response, json, error) in
             self.didFinish(json, self)
@@ -57,32 +56,32 @@ class waterwheelViewTableViewControllerNew<Row: Mappable, Cell: UITableViewCell>
         })
 
     }
-    override func viewDidLoad() {
+    override public func viewDidLoad() {
         super.viewDidLoad()
         tableView.registerClass(Cell.self, forCellReuseIdentifier: reuseIdentifier)
     }
 
-    override func didReceiveMemoryWarning() {
+    override public func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    override public func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override public func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return rows.count
     }
 
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    override public func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let item = rows[indexPath.row]
         didSelect(item)
     }
 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    override public func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath) as! Cell
         let item = rows[indexPath.row]
         configure(cell, item)
